06222e4c26aa3d4c256ccf468c40da80c930535a
Klayton Kowalski <klaytonkowalski@gmail.com>
2021-03-29 15:54:02 -0400

    Changed thumbnail.

--- example/simplex.lua
+++ example/simplex.lua
@@ -9,7 +9,6 @@ local table = table
 local tonumber = tonumber
 local ipairs = ipairs
 local error = error
-local bit = require("bit")
 
 local simplex = {}
 
@@ -340,7 +339,7 @@ function simplex.Noise4D(x,y,z,w)
 	-- To find out which of the 24 possible simplices we're in, we need to
 	-- determine the magnitude ordering of x0, y0, z0 and w0.
 	-- The method below is a good way of finding the ordering of x,y,z,w and
-	-- then find the correct traversal order for the simplex we’re in.
+	-- then find the correct traversal order for the simplex weï¿½re in.
 	-- First, six pair-wise comparisons are performed between each possible pair
 	-- of the four coordinates, and the results are used to add up binary bits
 	-- for an localeger index.
